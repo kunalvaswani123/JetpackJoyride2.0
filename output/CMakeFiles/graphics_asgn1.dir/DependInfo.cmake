b2a642417ed5bb8dc80e41739c027f8f81ac5111
Kunal Vaswani <kvaswani2012@gmail.com>
2019-07-22 13:22:02 +0530

    to run the files

---
+++ output/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/7-segment.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/7-segment.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/ball.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/beam.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/beam.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/boomerang.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/boomerang.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/coin.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/coin.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/color.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/dragon.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/dragon.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/ice.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/ice.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/input.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/line.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/line.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/magnet.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/magnet.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/main.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/nonedit.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/other_handlers.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/platform.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/platform.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/powerup.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/powerup.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/powerup2.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/powerup2.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/prop.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/prop.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/ring.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/ring.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/timer.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/wall.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/wall.cpp.o"
+  "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/src/water.cpp" "/home/kunal/Sem4/Graphics/Assignment1/graphics-boilerplate/output/CMakeFiles/graphics_asgn1.dir/src/water.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
