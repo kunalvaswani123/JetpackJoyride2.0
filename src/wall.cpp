2142934163d182a7061e6e48089bf98f1c19f6b1
Kunal Vaswani <kvaswani2012@gmail.com>
2019-07-22 13:20:31 +0530

    adding files

---
+++ src/wall.cpp
@@ -0,0 +1,82 @@
+#include "wall.h"
+#include "main.h"
+
+Wall::Wall(float x, float y) {
+    this->position = glm::vec3(x, y, 0);
+    // this->rotation = 0;
+    // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+    static GLfloat vertex_buffer_data[] = {
+        -16.0f, 16.0f, -5.0f, 
+        -16.0f, 8.0f, -5.0f,
+        16.0f, 8.0f, -5.0f,
+        16.0f, 8.0f, -5.0f, 
+        16.0f, 16.0f, -5.0f,
+        -16.0f, 16.0f, -5.0f,
+        -16.0f, 8.0f, -5.0f, 
+        -16.0f, 1.0f, -5.0f,
+        16.0f, 1.0f, -5.0f,
+        16.0f, 1.0f, -5.0f, 
+        16.0f, 8.0f, -5.0f,
+        -16.0f, 8.0f, -5.0f,
+        -16.0f, 1.0f, -5.0f, 
+        -16.0f, -8.0f, -5.0f,
+        16.0f, -8.0f, -5.0f,
+        16.0f, -8.0f, -5.0f, 
+        16.0f, 1.0f, -5.0f,
+        -16.0f, 1.0f, -5.0f,
+        -16.0f, -8.0f, -5.0f, 
+        -16.0f, -16.0f, -5.0f,
+        16.0f, -16.0f, -5.0f,
+        16.0f, -16.0f, -5.0f, 
+        16.0f, -8.0f, -5.0f,
+        -16.0f, -8.0f, -5.0f           
+    };
+    static GLfloat g_color_buffer_data[] = {
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.6f, 0.98f, 0.6f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f,
+        0.56f, 0.92f, 0.56f        
+    };
+    this->object = create3DObject(GL_TRIANGLES, 8 * 3, vertex_buffer_data, g_color_buffer_data, GL_FILL);
+}
+
+void Wall::draw(glm::mat4 VP) {
+    Matrices.model = glm::mat4(0.2f);
+    glm::mat4 translate = glm::translate (this->position);    // glTranslatef
+    // glm::mat4 rotate    = glm::rotate((float) (this->rotation * M_PI / 180.0f), glm::vec3(1, 0, 0));
+    // No need as coords centered at 0, 0, 0 of cube arouund which we waant to rotate
+    // rotate          = rotate * glm::translate(glm::vec3(0, -0.6, 0));
+    Matrices.model *= (translate);
+    glm::mat4 MVP = VP * Matrices.model;
+    glUniformMatrix4fv(Matrices.MatrixID, 1, GL_FALSE, &MVP[0][0]);
+    draw3DObject(this->object);
+}
+
+void Wall::set_position(float x, float y) {
+    this->position = glm::vec3(x, y, 0);
+}
+
+void Wall::tick() {
+
+}
